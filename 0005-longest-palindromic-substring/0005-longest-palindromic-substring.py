08d8a135aaf15d805aa8b6b6835c8d52fd99254d
Kim Dael <daelkdev@gmail.com>
2023-12-05 01:26:24 +0900

    12/4 피드백 적용

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -16,3 +16,20 @@ class Solution:
             if len(even) > len(res):
                 res = even
         return res
+    
+# 12/5 피드백 적용
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def getMaximumPalindrome(l, r):
+            while(l >= 0 and r < len(s) and s[l] == s[r]):
+                l -= 1
+                r += 1
+            return s[l+1:r]
+        
+        res = ""
+        for i in range(len(s)):
+            odd = getMaximumPalindrome(i, i)
+            even = getMaximumPalindrome(i, i+1)
+            
+            res = max(odd, even, res, key=len)
+        return res
