b3596f24596a1f21e774ebf09c510ab121355d57
Kim Dael <daelkdev@gmail.com>
2023-12-06 02:15:50 +0900

    피드백 반영

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -2,17 +2,14 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s
-        zigzag = [[] for _ in range(numRows+1)]
-        idx = 1
+        zigzag = [[] for _ in range(numRows)]
+        idx = 0
         change = 1
         for x in s:
             zigzag[idx].append(x)
-            if idx == numRows:
+            if idx == numRows-1:
                 change = -1
-            elif idx == 1:
+            elif idx == 0:
                 change = 1
             idx += change
-        res = ""
-        for i in range(1, numRows+1):
-            res += "".join(zigzag[i])
-        return res
+        return "".join("".join(row) for row in zigzag)
