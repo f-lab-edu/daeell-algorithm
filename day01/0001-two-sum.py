73cb133c712d2f34dd11ea16495ab7f2058f4c66
김다엘 <104256143+Daeell@users.noreply.github.com>
2023-12-17 22:59:04 +0900

    day01_11/28 (#1)
    * day01_11/28
    
    * day02_11/29
    
    * day02_11/29
    
    * new solution
    
    * 정렬+이진탐색
    
    * Update 0002-add_two_numbers.py
    
    * Update 0002-add_two_numbers.py
    
    * Update 0002-add_two_numbers.py
    
    * Space Complexity O(1), Time Complexity O(n^2)

---
+++ day01/0001-two-sum.py
@@ -0,0 +1,48 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict_nums = {num : idx for idx, num in enumerate(nums)}
+        for idx, num in enumerate(nums):
+            find_num = target - num
+            if find_num in dict_nums and dict_nums[find_num] != idx:
+                return [idx, dict_nums[find_num]]
+
+# 정렬 + 이진탐색            
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_and_idx = [(num, idx)for idx, num in enumerate(nums)]
+        num_and_idx.sort()
+        for i in range(len(nums)):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if num_and_idx[mid][0] == target - num_and_idx[i][0]:
+                    if mid != i: # 두 요소가 서로 다른 인덱스를 가질 때만 반환
+                        return sorted([num_and_idx[i][1], num_and_idx[mid][1]])
+                    else: # 같은 요소를 참조하는 경우, 이진 탐색 계속
+                        if mid + 1 < len(nums) and num_and_idx[mid + 1][0] == target - num_and_idx[i][0]:
+                            return sorted([num_and_idx[i][1], num_and_idx[mid + 1][1]])
+                        elif mid - 1 >= 0 and num_and_idx[mid - 1][0] == target - num_and_idx[i][0]:
+                            return sorted([num_and_idx[i][1], num_and_idx[mid - 1][1]])
+                        break
+                elif num_and_idx[mid][0] < target - num_and_idx[i][0]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return None
+    
+# Space Complexity O(1)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res = 0  # O(1)
+        idx1, idx2 = 0, 0  # O(1)
+
+        for idx, num in enumerate(nums):  # O(n)
+            res = target - num  # O(1)
+            idx1 = idx  # O(1)
+
+            if res in nums:  # 최악의 경우 O(n),
+                idx2 = nums.index(res)  # 최악의 경우 O(n),
+                if idx2 != idx1:  # O(1)
+                    return [idx1, idx2]  # O(1)
+
+        return None
