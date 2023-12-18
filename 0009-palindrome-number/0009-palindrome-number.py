02d056b302e4995cfb864729561ecb8fa35ca34b
Kim Dael <daelkdev@gmail.com>
2023-12-18 22:15:14 +0900

    12/18

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -17,3 +17,18 @@ class Solution:
             x //= 10
 
         return x == reverted_number or x == reverted_number // 10
+    
+# 12/18 
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        str_x = str(x)
+        length = len(str_x)
+
+        for i in range(length // 2):
+            if str_x[i] != str_x[length - 1 - i]:
+                return False
+
+        return True
