19f944f106d06ef261d062b2b42940a93005b250
Kim Dael <daelkdev@gmail.com>
2023-12-18 23:52:50 +0900

    12/18 풀이 수정

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -12,7 +12,8 @@ class Solution:
             if s[i] == " ":
                 self.reverse(s, start, i - 1)  # O(k) k는 해당 단어의 길이
                 start = i + 1
-        self.reverse(s, start, i)  # O(m) m은 해당 단어의 길이
+        # self.reverse(s, start, i)  # O(m) m은 해당 단어의 길이
+        self.reverse(s, start, len(s) - 1)
         self.reverse(s, 0, len(s) - 1)  # O(n)
 
         return " ".join("".join(s).split())  # O(n)
--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -15,3 +15,23 @@ class Solution:
             r -= 1
 
         return "".join(s)  # O(n)
+    
+# 12/18 set을 활용하여 검색 속도 최적화
+
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"])
+        l, r = 0, len(s) - 1
+        s = list(s)
+
+        while l < r:  # O(n)은 len(s)
+            while l < r and s[l] not in vowels:  # O(1)
+                l += 1
+            while l < r and s[r] not in vowels:  # O(1)
+                r -= 1
+
+            s[l], s[r] = s[r], s[l]  # O(1)
+            l += 1
+            r -= 1
+
+        return "".join(s)  # O(n)
