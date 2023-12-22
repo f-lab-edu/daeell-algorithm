e593bc342befd77c68bc28885babf674911ca32b
김다엘 <104256143+Daeell@users.noreply.github.com>
2023-12-23 02:39:30 +0900

    12/10 일요일 (#11)
    * 12/10 일요일
    
    * 12/18 풀이 수정

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, s: list, start, end):
+        while start < end:
+            s[start], s[end] = s[end], s[start]
+            start += 1
+            end -= 1
+
+    def reverseWords(self, s: str) -> str:
+        s = list(s)
+        start = 0
+        for i in range(len(s)):  # O(n)
+            if s[i] == " ":
+                self.reverse(s, start, i - 1)  # O(k) k는 해당 단어의 길이
+                start = i + 1
+        # self.reverse(s, start, i)  # O(m) m은 해당 단어의 길이
+        self.reverse(s, start, len(s) - 1)
+        self.reverse(s, 0, len(s) - 1)  # O(n)
+
+        return " ".join("".join(s).split())  # O(n)
