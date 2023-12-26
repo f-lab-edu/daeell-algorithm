e9749211668abedf5bafc072abd1831e94d01490
Kim Dael <daelkdev@gmail.com>
2023-12-27 02:36:33 +0900

    12/26 746

---
+++ 1226_746/746.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        length = len(cost)
+
+        for i in range(2, length):
+            cost[i] = min(cost[i - 1], cost[i - 2]) + cost[i]
+
+        return min(cost[-1], cost[-2])
